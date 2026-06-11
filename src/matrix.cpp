09cdb1a9aa68f9e3936ae8f7d8382f7c0b05b723
Gemo555 <15339687962@163.com>
2026-06-12 02:02:03 +0800

    feat: implement transpose and matmul

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,17 +1,8 @@
 #include "tinytensor/matrix.hpp"
 
 #include <stdexcept>
-#include <string>
 
 namespace tinytensor {
-
-namespace {
-
-[[noreturn]] void throw_handwrite_todo(const char* function_name) {
-    throw std::logic_error(std::string(function_name) + ": TODO(handwrite)");
-}
-
-}  // namespace
 
 Matrix::Matrix(std::size_t rows, std::size_t cols) {
     rows_ = rows;
@@ -76,27 +67,32 @@ Matrix Matrix::ones(std::size_t rows, std::size_t cols) {
 }
 
 Matrix Matrix::transpose() const {
-    // TODO(handwrite):
-    //   请你自己实现这里。
-    //   思考：
-    //   1. 输出矩阵的 shape 应该是什么？
-    //   2. 输入矩阵的哪个元素会映射到输出矩阵的哪个元素？
-    //   3. 当前版本应该复制数据，还是创建 view？
-    //   在你能解释清楚设计之前，不要让 AI 直接补完这个函数。
-    throw_handwrite_todo("Matrix::transpose()");
+    Matrix m(cols_, rows_);
+
+    for (std::size_t i = 0; i < rows_; ++i) {
+        for (std::size_t j = 0; j < cols_; ++j) {
+            m(j, i) = (*this)(i, j);
+        }
+    }
+
+    return m;
 }
 
 Matrix Matrix::matmul(const Matrix& other) const {
-    (void)other;
-    // TODO(handwrite):
-    //   请你自己实现这里。
-    //   思考：
-    //   1. A.matmul(B) 对维度有什么要求？
-    //   2. 维度不匹配时应该发生什么？
-    //   3. 朴素矩阵乘法的三层循环分别是什么？
-    //   4. 哪种内存访问模式更适合缓存？
-    //   在你能解释清楚设计之前，不要让 AI 直接补完这个函数。
-    throw_handwrite_todo("Matrix::matmul(other)");
+    if (cols_ != other.rows_) {
+        throw std::invalid_argument("Matrix dimensions do not match for matmul");
+    }
+
+    Matrix m = zeros(rows_, other.cols_);
+    for (std::size_t i = 0; i < rows_; ++i) {
+        for (std::size_t k = 0; k < cols_; ++k) {
+            for (std::size_t j = 0; j < other.cols_; ++j) {
+                m(i, j) += (*this)(i, k) * other(k, j);
+            }
+        }
+    }
+
+    return m;
 }
 
 }  // namespace tinytensor
